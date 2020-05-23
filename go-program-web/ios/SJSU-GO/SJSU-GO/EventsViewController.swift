8214b8d834e81708efe987c587dc27b8486c629d
prathamr <prathamesh.karve@gmail.com>
2020-05-22 18:40:03 -0700

    Re-organizing folder structure

--- go-program-web/ios/SJSU-GO/SJSU-GO/EventsViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EventsViewController.swift
-//  SJSU-GO
-//
-//  Created by prkarve on 5/18/20.
-//  Copyright © 2020 SJSU. All rights reserved.
-//
-
-import Foundation
--- go-program-web/ios/SJSU-GO/SJSU-GO/RedeemViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RedeemViewController.swift
-//  SJSU-GO
-//
-//  Created by prkarve on 5/18/20.
-//  Copyright © 2020 SJSU. All rights reserved.
-//
-
-import Foundation
